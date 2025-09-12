06101bcfe492301e40a19f94a39fae76db81312c
Td hac <twodhacker@gmail.com>
2025-09-12 08:49:34 +0630

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests,json,os
 from bs4 import BeautifulSoup
 app=FastAPI()
 @app.get("/")
- def twod_live():
+ def home():
     url =     "https://www.set.or.th/en/market/product/stock/overview"   
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
