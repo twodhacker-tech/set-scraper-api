a4e162ef38c9fa851ccd7b9641b4ea90044c3469
Td hac <twodhacker@gmail.com>
2025-09-12 16:31:34 +0630

    main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 
 @app.get("/")
 def twod():
+    #1.Date and time
+     mm_date = datetime.now(pytz.timezone("Asia/Yangon")).strftime("%Y-%m-%d ")
+   mm_time = datetime.now(pytz.timezone("Asia/Yangon")).strftime("%H:%M:%S")
+   #2.bs4 scraper 
     url = "https://www.set.or.th/en/market/product/stock/overview"   
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -16,12 +20,13 @@ def twod():
     set_index=table.find_all("div")[4]
     value_index=table.find_all("div")[6]
     Live_set=set_index.string
-    Live_value=value_index.string
+    Liver_value=value_index.string
     return {
-
+            "date":mm_date,
+            "time":mm_time,
         "Live": {
             "set": Live_set.strip(),
-            "value": Live_value.strip(),
+            "value": Liver_value.strip(),
             "fetched_at": int(time.time())
         }
     }
