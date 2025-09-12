b865042660ea8b9e9d530e0b89d37351d99ef93c
Td hac <twodhacker@gmail.com>
2025-09-12 08:53:29 +0630

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from datetime import datetime
 from pytz import timezone
-import requests,json,os
+import requests
 from bs4 import BeautifulSoup
-app=FastAPI()
+import time
+
+app = FastAPI()
+
 @app.get("/")
- def home():
-    url =     "https://www.set.or.th/en/market/product/stock/overview"   
+def home():
+    url = "https://www.set.or.th/en/market/product/stock/overview"   
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     table = soup.find_all("table")[1]
@@ -14,10 +17,11 @@ app=FastAPI()
     value_index=table.find_all("div")[6]
     Live_set=set_index.string
     Liver_value=value_index.string
-    
     return {
-        "live": {
+
+        "Live": {
             "set": Live_set.strip(),
-            "value": Live_value.strip()},
-        "fetched_at": int(time.time())
+            "value": Liver_value.strip(),
+            "fetched_at": int(time.time())
         }
+    }
