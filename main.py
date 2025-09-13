93de9c95883d49182b4b8c9539a0905aa0e4c8b4
Td hac <twodhacker@gmail.com>
2025-09-13 10:49:41 +0630

    main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pytz
 import time
 import requests
 from bs4 import BeautifulSoup
+import math
 
 app = FastAPI()
 
@@ -21,7 +22,19 @@ def twod():
     live_set = set_index.get_text(strip=True)
     live_value = value_index.get_text(strip=True)
 
-    # Myanmar Timezone
+    clean_set = live_set.replace(",","")
+    extract_set = float(clean_set)
+    top = str(extract_set)[-1]
+ 
+     clean_value = live_value.replace(",","")
+     extract_value = float(clean_value)
+     last = str(int(extract_value))[-1]
+    
+       twod_live=int(f"{top}{last}")
+
+    
+
+# Myanmar Timezone
     mm_time = datetime.now(pytz.timezone("Asia/Yangon"))
     mm_date = mm_time.strftime("%Y-%m-%d")
     mm_time_str = mm_time.strftime("%H:%M:%S")
@@ -30,6 +43,7 @@ def twod():
         "date": mm_date,
         "time": mm_time_str,
         "Live": {
+            "twod":twod_live,
             "set": live_set,
             "value": live_value,
             "fetched_at": int(time.time())
