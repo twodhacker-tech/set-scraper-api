4b4bef222e85ee96549d2cd1d158e16c4bc2fdd0
Td hac <twodhacker@gmail.com>
2025-09-13 09:28:50 -0700

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 app=FastAPI()
 @app.get("/")
 def fetch_live_data():
-    url = "https://www.set.or.th/en/market/product/stock/overview"
+    url = "THA_LIN"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
