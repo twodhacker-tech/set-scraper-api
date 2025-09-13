91636b78d6eb4deefc975bbad3901369ae711a32
Td hac <twodhacker@gmail.com>
2025-09-13 09:37:10 -0700

    api.py

--- api.py
+++ api.py
@@ -7,34 +7,7 @@ app = FastAPI()
 def fetch_live_data():
     LINK_D = "THA_LIN"
     response = requests.get(LINK_D)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    table = soup.find_all("table")[1]
-    set_index = table.find_all("div")[4]
-    value_index = table.find_all("div")[6]
-
-    live_set = set_index.get_text(strip=True)
-    live_value = value_index.get_text(strip=True)
-
-    clean_set = live_set.replace(",", "")
-    top = str(float(clean_set))[-1]
-
-    clean_value = live_value.replace(",", "")
-    last = str(int(float(clean_value)))[-1]
-
-    twod_live = str(int(f"{top}{last}"))
-
-    mm_time = datetime.now(pytz.timezone("Asia/Yangon"))
-    return {
-        "date": mm_time.strftime("%Y-%m-%d"),
-        "time": mm_time.strftime("%H:%M:%S"),
-        "Live": {
-            "twod": twod_live,
-            "set": live_set,
-            "value": live_value,
-            "fetched_at": int(time.time())
-        }
-    }
+    TWODHACKER
 
 @app.get("/")
 def home():
