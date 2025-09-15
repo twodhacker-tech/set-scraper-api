8788c041eac79e73cac8425f489660d6bac3d17d
Td hac <twodhacker@gmail.com>
2025-09-15 08:51:40 -0700

    main.py

--- main.py
+++ main.py
@@ -103,4 +103,6 @@ def root():
     return jsonify({"message": "Flask SET 2D API is running"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
