ab1a19a443b0a024fbd6946568c95210eba2ad62
Td hac <twodhacker@gmail.com>
2025-09-15 12:55:00 +0630

    api.py

--- api.py
+++ api.py
@@ -23,7 +23,7 @@ def fetch_live_data():
     clean_value = live_value.replace(",", "")
     last = str(int(float(clean_value)))[-1]
 
-    twod_live = str(int(f"{top}{last}"))
+    twod_live = f"{top}{last}"
 
     mm_time = datetime.now(pytz.timezone("Asia/Yangon"))
     return {
