6ca95530eee162bcd2b167dc00db4e1d10caac14
Td hac <twodhacker@gmail.com>
2025-09-15 12:36:20 +0630

    api.py

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ def fetch_live_data():
     live_value = value_index.get_text(strip=True)
 
     clean_set = live_set.replace(",", "")
-    top = str(float(clean_set))[-1]
+    top = str(clean_set)[-1]
 
     clean_value = live_value.replace(",", "")
     last = str(int(float(clean_value)))[-1]
