80839e41bc6996d95de2f4ace20e8c090bc4cc80
Td hac <twodhacker@gmail.com>
2025-09-16 08:57:20 +0630

    main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def get_live():
     url = "https://www.set.or.th/en/market/product/stock/overview"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
+
     table = soup.find_all("table")[1]
     set_index = table.find_all("div")[4]
     value_index = table.find_all("div")[6]
@@ -35,10 +36,12 @@ def get_live():
     live_set = set_index.get_text(strip=True)
     live_value = value_index.get_text(strip=True)
 
-    top = "{:.2f}".format(float(live_set.replace(",","")))[-1]
-last = str(int(float(live_value)))[-1]
-if last == "-" or last == "":
-    last = "0"
+    clean_set = live_set.replace(",", "")
+    formatted="{:.2f}".format(float(clean_set))
+    top = formatted[-1]
+
+    clean_value = live_value.replace(",", "")
+    last = str(int(float(clean_value)))[-1]
 
     twod_live = f"{top}{last}"
 
